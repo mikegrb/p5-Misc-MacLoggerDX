a988e0317767b9d636528c04aa3a7462b562fd42
Michael Greb <michael@thegrebs.com>
2015-12-25 23:37:01 -0500

    many improvements to udp-tune
    1. Mode mapping for PSK31 -> BPSK31
    2. Keep track of 2000 most recent frequency to call sign mapping & pre-fill fldigi log call
    3. Better decoding of the packets

--- udp-tune.pl
+++ udp-tune.pl
@@ -4,45 +4,82 @@ use 5.014;
 use strict;
 use autodie;
 
+use Cache::LRU;
 use IO::Socket::INET;
 use RPC::XML::Client;
 use Data::Printer;
 
-my $OFFSET    = 1500;
-my $MLDX_NAME = "Michael’s MacBook Pro";
+my $OFFSET     = 1500;
+my $MLDX_NAME  = "Michael’s MacBook Pro";
+my $FREQ_CACHE = 2000;
+
+my %MODE_MAP = (
+  PSK31 => 'BPSK31',
+);
 
 my $fldigi = RPC::XML::Client->new('http://127.0.0.1:7362/');
 my $sock = IO::Socket::INET->new(
   LocalPort => 9932,
   Proto     => 'udp'
 ) or die;
+my $cache = Cache::LRU->new( size => $FREQ_CACHE );
 
 my $buffer;
-while($sock->recv($buffer = '', 9000)) {
-  # 000107 Michael’s MacBook Pro [SpotTune:RxMHz:3.56200, TxMHz:3.56200, Band:80M, Mode:CW]
-  if ($buffer =~ m/^\d+ \Q$MLDX_NAME\E \[SpotTune:RxMHz:([^,]+), TxMHz:([^,]+), Band:(\d+)M, Mode:([^\]]+)\]$/) {
-    my ($rx, $tx, $band, $mode) = ($1, $2, $3, $4);
-    say "Tune request for $mode on $band at $rx";
-    tune_fldigi( $rx, $mode );
-  }
-  elsif ($buffer =~ m/Spot Report/) {
-    # too many of these to print them all
-    next
+while ( $sock->recv( $buffer = '', 9000 ) ) {
+  if ( $buffer =~ m/^\d+ \Q$MLDX_NAME\E \[(.*)\]$/ ) {
+
+    my ( $type, $content ) = split /:/, $1, 2;
+
+    my %data;
+    for my $data ( split /, /, $content ) {
+      my ( $key, $value ) = split /:/, $data;
+      $data{$key} = $value;
+    }
+
+    if ( $type eq 'SpotTune' ) {
+      my $call = $cache->get( $data{RxMHz} ) || '';
+      say "Tune request for $call $data{Mode} on $data{Band} at $data{RxMHz}";
+      tune_fldigi( $data{RxMHz}, $data{Mode}, $call );
+    }
+
+    elsif ( $type eq 'Spot Report' ) {
+      $cache->set( $data{RxMHz}, $data{Call} );
+    }
+
+    elsif ( $type eq 'Lookup Report' ) {
+    }
+
+    else {
+      say "Received $type packet:";
+      p %data;
+    }
   }
   else {
-    say $buffer;
+    say "Unrecognized Packet: $buffer";
   }
 }
 
 sub tune_fldigi {
-  my ( $freq_mhz, $mode ) = @_;
+  my ( $freq_mhz, $mode, $call ) = @_;
+
+  $mode = $MODE_MAP{$mode} if exists $MODE_MAP{$mode};
+
   my $target_hz = ( $freq_mhz * 1000 * 1000 ) - $OFFSET;
-  my $prev_freq = $fldigi->send_request( 'main.set_frequency', $target_hz . '.0' );
-  my $prev_mode = $fldigi->send_request( 'modem.set_by_name', $mode );
-  my $offset    = $fldigi->send_request( 'modem.set_carrier', $OFFSET );
-  for ( $prev_freq, $prev_mode, $offset ) {
-    die "Error $_\n" unless ref $_;
-    p $_;
-    $_ = $_->value;
+
+  my %r;
+  $r{prev_freq} = $fldigi->send_request( 'main.set_frequency', $target_hz . '.0' );
+  $r{prev_mode} = $fldigi->send_request( 'modem.set_by_name', $mode );
+  $r{offset}    = $fldigi->send_request( 'modem.set_carrier', $OFFSET );
+  $r{clear}     = $fldigi->send_request( 'log.clear' );
+  $r{call}  = $fldigi->send_request( 'log.set_call', $call ) if $call;
+
+  for my $key ( keys %r ) {
+    next unless $r{$key};
+    if (! ref $r{$key}) {
+      say "Error for $key: $r{$key}";
+    }
+    elsif ( $r{$key} =~ m/ / ) {
+      say "Fldigi Error for $key: $r{$key}->value";
+    }
   }
 }
